9a75a26dc9d13abd1e4aed7dc099792b74650fdd
fullzer4 <53914594+fullzer4@users.noreply.github.com>
2022-03-31 11:29:10 -0300

    att

---
+++ teste1.py
@@ -0,0 +1,12 @@
+from re import M
+from tkinter import *
+
+master = Tk() #criar uma janela
+
+master.title("Matrizes") #titulo
+#master.iconbitmap(default="icone") #mudar o icone
+master.geometry("600x500+400+100") # largura x altura + dist esq + dist top ("onde vai iniciar o programa")
+
+#eventos
+
+master.mainloop() #rodar
